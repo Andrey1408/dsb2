48836697a6875af4e8e8df1f2d5cbfac440ea874
z000000 <44620649+z0-0-0@users.noreply.github.com>
2024-11-14 21:27:29 +0300

    nonblocking

--- ipc.c
+++ ipc.c
@@ -4,48 +4,65 @@
 #include "ipc.h"
 #include "pipe_utils.h"
 
-int send(void * self, local_id dst, const Message * msg) 
+int send(void *self, local_id dst, const Message *msg)
 {
-    pipe_ut * proc = self;
-    if (write(getWriterById(proc->cur_id, dst, proc), msg, sizeof(MessageHeader) + msg->s_header.s_payload_len) != -1) {
+    pipe_ut *proc = self;
+    if (write(getWriterById(proc->cur_id, dst, proc), msg, sizeof(MessageHeader) + msg->s_header.s_payload_len) != -1)
+    {
         return 0;
     }
     return -1;
 }
 
-int send_multicast(void * self, const Message * msg) 
+int send_multicast(void *self, const Message *msg)
 {
-    pipe_ut * proc = self;
-    for ( local_id i = 0; i < proc->size; i++ ) {
-        if ( i != proc->cur_id ) {
-            if ( send( proc, i, msg ) == -1 ) {
+    pipe_ut *proc = self;
+    for (local_id i = 0; i < proc->size; i++)
+    {
+        if (i != proc->cur_id)
+        {
+            if (send(proc, i, msg) == -1)
+            {
                 return -1;
             }
         }
-            
     }
     return 0;
 }
 
-int receive(void * self, local_id from, Message * msg) 
+int receive(void *self, local_id from, Message *msg)
 {
-    pipe_ut * proc = self;
-    if (read(getReaderById(proc->cur_id, from, proc), msg, sizeof(MessageHeader) + msg->s_header.s_payload_len) != -1 || read(getReaderById(proc->cur_id, from, proc), msg, sizeof(MessageHeader) + msg->s_header.s_payload_len) != 0) {
+    pipe_ut *proc = self;
+    int status;
+    status = read(getReaderById(proc->cur_id, from, proc), msg, sizeof(MessageHeader) + msg->s_header.s_payload_len) != -1 || read(getReaderById(proc->cur_id, from, proc), msg, sizeof(MessageHeader) + msg->s_header.s_payload_len);
+    if (status > 0)
+    {
         return 0;
     }
-    return -1;
+    else
+    {
+        return 1;
+    }
 }
 
-int receive_any(void * self, Message * msg) 
+int receive_any(void *self, Message *msg)
 {
-    pipe_ut * proc = self;
-    while (1) {
-        for ( local_id i = 0; i < proc->size; i++ ) {
-            if ( i != proc->cur_id ) {
-                if ( receive( self, i, msg ) != -1 ) {
+    pipe_ut *proc = self;
+    while (1)
+    {
+        for (local_id i = 0; i < proc->size; i++)
+        {
+            if (i != proc->cur_id)
+            {
+                if (receive(self, i, msg) == 1)
+                {
+                    continue;
+                }
+                else
+                {
                     return 0;
-                }               
+                }
             }
-        } 
+        }
     }
 }
--- pa23.c
+++ pa23.c
@@ -6,61 +6,66 @@
 #include "log.h"
 #include "banking.h"
 
-
-
-void transfer(void * parent_data, local_id src, local_id dst,
+void transfer(void *parent_data, local_id src, local_id dst,
               balance_t amount)
 {
     TransferOrder trnsfr = {
-        .s_src = src, 
-        .s_dst = dst, 
-        .s_amount = amount
-    };
+        .s_src = src,
+        .s_dst = dst,
+        .s_amount = amount};
 
-    Message message = create_message(TRANSFER, (void*) &trnsfr, sizeof(trnsfr));
+    Message message = create_message(TRANSFER, (void *)&trnsfr, sizeof(trnsfr));
 
     send(parent_data, src, &message);
 
     Message message_out;
     message_out.s_header.s_type = STARTED;
-
 }
 
-void balance_history (BalanceHistory* history, BalanceState state) {
-    
-    if (state.s_time >= history->s_history_len -1) {
-        for ( timestamp_t t = history->s_history_len; t < state.s_time; t++ ) {
-            history->s_history[t] = history->s_history[t-1];
+void balance_history(BalanceHistory *history, BalanceState state)
+{
+
+    if (state.s_time >= history->s_history_len - 1)
+    {
+        for (timestamp_t t = history->s_history_len; t < state.s_time; t++)
+        {
+            history->s_history[t] = history->s_history[t - 1];
             history->s_history[t].s_time = t;
         }
         history->s_history[state.s_time] = state;
         history->s_history_len = state.s_time + 1;
     }
-    if (state.s_time < history->s_history_len -1) {
+    if (state.s_time < history->s_history_len - 1)
+    {
         return;
     }
 }
 
-void wait_messages(pipe_ut* pp, MessageType status) 
+void wait_messages(pipe_ut *pp, MessageType status)
 {
     local_id counter = 0;
     local_id to_wait;
-    if (pp->cur_id == PARENT_ID) {
+    if (pp->cur_id == PARENT_ID)
+    {
         to_wait = pp->size - 1;
-    } else {
+    }
+    else
+    {
         to_wait = pp->size - 2;
     }
-    
-    while (counter < to_wait) {
+
+    while (counter < to_wait)
+    {
         Message msg;
-        receive_any( pp, &msg );
-        if (msg.s_header.s_type == status) {
+        receive_any(pp, &msg);
+        if (msg.s_header.s_type == status)
+        {
             counter++;
         }
-    } 
+    }
 }
 
-Message create_message ( MessageType type, void* contents, uint16_t size ) 
+Message create_message(MessageType type, void *contents, uint16_t size)
 {
     Message msg;
 
@@ -73,16 +78,18 @@ Message create_message ( MessageType type, void* contents, uint16_t size )
     return msg;
 }
 
-void transfer_process(pipe_ut* pp, Message* msg) 
+void transfer_process(pipe_ut *pp, Message *msg)
 {
-    TransferOrder* trnsfr = (TransferOrder*)msg->s_payload;
-    if (pp->cur_id == trnsfr->s_dst) {
+    TransferOrder *trnsfr = (TransferOrder *)msg->s_payload;
+    if (pp->cur_id == trnsfr->s_dst)
+    {
         pp->state.s_balance += trnsfr->s_amount;
         balance_history(&(pp->history), pp->state);
         send(pp, trnsfr->s_dst, msg);
         log_transfer_out(trnsfr);
     }
-    if (pp->cur_id == trnsfr->s_src) {
+    if (pp->cur_id == trnsfr->s_src)
+    {
         pp->state.s_balance -= trnsfr->s_amount;
         balance_history(&(pp->history), pp->state);
         Message msg = create_message(ACK, NULL, 0);
@@ -91,33 +98,53 @@ void transfer_process(pipe_ut* pp, Message* msg)
     }
 }
 
-void child_stopping(pipe_ut* pp, const int* processes_left_counter){
+void child_stopping(pipe_ut *pp, const int *processes_left_counter)
+{
     Message msg = create_message(DONE, NULL, 0);
-    send_multicast((void*) pp, &msg);
+    send_multicast((void *)pp, &msg);
     int i = *processes_left_counter;
-
-    for(int id = 1; i < pp->size - 2;id++) {
-        if(id != pp->cur_id){
-            receive(pp, id, &msg);
-            switch (msg.s_headr.s_type)
+    for (int id = 1; i < pp->size - 2 || id < pp->size - 1; id++)
+    {
+        if (id != pp->cur_id)
+        {
+            int status = receive(pp, id, &msg);
+            if (status == 0)
             {
-            case DONE:
-                close(pp->recepients[pp->cur_id][id])
-                break;
-            
-            default:
-                break;
+                switch (msg.s_headr.s_type)
+                {
+                case DONE:
+                    i++;
+                    continue;
+                case TRANSFER:
+                    pp->state.s_time = get_physical_time();
+                    transfer_process(pp, &msg);
+                    id = 0;
+                    continue;
+                default:
+                    continue;
+                }
             }
         }
     }
+    Message msg = create_message(DONE, NULL, 0);
+    send_multicast((void *)pp, &msg);
+    timestamp_t end_time = get_physical_time();
+    pp->state = pp->history.s_history[pp->history.s_history_len - 1];
+    pp->state.s_time = end_time;
+    balance_history(&(pp->history), pp->state);
+    log_done(pp);
+    uint16_t p_size = (pp->history.s_history_len) * sizeof(BalanceState) + sizeof(pp->history.s_history_len) + sizeof(pp->history.s_id);
+    msg = create_message(BALANCE_HISTORY, &pp->history, p_size);
+    send(pp, PARENT_ID, &msg);
 }
 
-void child_work(pipe_ut* pp) 
+void child_work(pipe_ut *pp)
 {
     Message msg = create_message(STARTED, NULL, 0);
-    send(pp, PARENT_ID,  &msg);
+    send(pp, PARENT_ID, &msg);
     int i = 0;
-    while (i < pp->size - 2) {
+    while (i < pp->size - 2)
+    {
         Message msg;
         receive_any(pp, &msg);
 
@@ -137,20 +164,12 @@ void child_work(pipe_ut* pp)
             continue;
         }
     }
-    timestamp_t end_time = get_physical_time();
-    pp->state = pp->history.s_history[pp->history.s_history_len-1];
-    pp->state.s_time = end_time;
-    balance_history(&(pp->history), pp->state);
-    log_done(pp);
-    uint16_t p_size = (pp->history.s_history_len) * sizeof(BalanceState) + sizeof(pp->history.s_history_len) + sizeof(pp->history.s_id);
-    Message msg_to_p = create_message(BALANCE_HISTORY, &pp->history, p_size);
-    send(pp, PARENT_ID, &msg_to_p);
 }
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-    //bank_robbery(parent_data);
-    //print_history(all);
+    // bank_robbery(parent_data);
+    // print_history(all);
 
     return 0;
 }
